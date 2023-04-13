28699a4e7bc73585e435a2b565f83508472896b6
Vladislav Gainutdinov <gainutdinov.vlad@mail.ru>
2023-04-13 21:30:27 +0300

    test.py int float only

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 def sum_two_number(a, b):
     """Сумма переменных"""
-    return a + b
 
+    access_type_list = [int, float]
+    if type(a) in access_type_list and type(b) in access_type_list:
+        return a + b
+    else:
+        return False
 
 # определение переменных
 a = 5
-b = 6
+b = 7.5
 
 # расчет
 print(sum_two_number(a, b))
